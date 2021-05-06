347438057dd9dbed692210b1243d32c819390799
Nabendu <nabendu.biswas@gmail.com>
2021-05-06 12:19:36 +0530

    Final commit

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const { cloudinary } = require('./utils/cloudinary');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+app.get('/api/images', async (req, res) => {
+    const { resources } = await cloudinary.search
+        .expression('folder:cloudinary_react')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
+
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
+});
+
+app.post('/api/upload', async (req, res) => {
+    try {
+        const fileStr = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'cloudinary_react',
+        });
+        console.log(uploadResponse);
+        res.json({ msg: 'File uploaded sucessfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log('listening on 3001');
+});
---
+++ frontend/src/pages/Upload.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+
+const Upload = () => {
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+        setSelectedFile(file);
+        setFileInputState(e.target.value);
+    };
+
+    const handleSubmitFile = (e) => {
+        e.preventDefault();
+        if (!selectedFile) return;
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+            uploadImage(reader.result);
+        };
+        reader.onerror = () => {
+            console.error('Error');
+        };
+    };
+
+    const uploadImage = async (base64EncodedImage) => {
+        try {
+            await fetch('/api/upload', {
+                method: 'POST',
+                body: JSON.stringify({ data: base64EncodedImage }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            setFileInputState('');
+            setPreviewSource('');
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        };
+    };
+
+
+    return (
+        <div>
+            <h1>Upload</h1>
+            <form onSubmit={handleSubmitFile} className="form">
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
+        </div>
+    )
+}
+
+export default Upload
